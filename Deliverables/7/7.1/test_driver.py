d146bfed3d4224645031dee2bff4bd53be78a54c
pahul-singh25 <55995639+pahul-singh25@users.noreply.github.com>
2019-11-12 16:39:05 -0600

    for test

--- Deliverables/7/7.1/test_driver.py
+++ Deliverables/7/7.1/test_driver.py
@@ -60,7 +60,7 @@ def main():
             else:
                 conn.send(pickle.dumps(i))
                 ret.append(pickle.loads(conn.recv(4096)))
-    except EOFError or BrokenPipeError:
+    except (EOFError, BrokenPipeError) as e:
         pass
 
     
